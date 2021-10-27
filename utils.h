28b843875fdcc444c51316c700cc8cb9ad3fd018
A. Ahani <alirezaahani@protonmail.com>
2021-10-27 11:28:58 +0330

    Update utils.h

--- utils.h
+++ utils.h
@@ -5,5 +5,6 @@ char* utils_strin();
 int utils_variance(int arr[], int n);
 int utils_frequent(int arr[], int n);
 bool* utils_sieve_prime(uint64_t n);
+bool utils_is_prime(uint64_t n);
 
 #endif // UTILS_H_INCLUDED
